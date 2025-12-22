0297d74a257c1eafbe58d48b0ef5a2f539c88f38
AnastazijaPetrov <anastazija.petrov004@gmail.com>
2025-12-22 23:52:44 +0100

    Add profile picture editing to passenger/admin personal info page

--- frontend/GetGo/src/app/passenger/passenger-profile-info/passenger-profile-info.ts
+++ frontend/GetGo/src/app/passenger/passenger-profile-info/passenger-profile-info.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { UserNavBarComponent } from '../../layout/user-nav-bar/user-nav-bar.component';
 
 @Component({
@@ -8,5 +8,33 @@ import { UserNavBarComponent } from '../../layout/user-nav-bar/user-nav-bar.comp
   styleUrl: './passenger-profile-info.css',
 })
 export class PassengerProfileInfo {
+  profileImageUrl: string = 'assets/images/pfp.png';
+  selectedFile: File | null = null;
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  onImageClick() {
+    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+    fileInput.click();
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      this.selectedFile = input.files[0];
+      
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.profileImageUrl = e.target.result;
+        this.cdr.detectChanges();
+      };
+      reader.readAsDataURL(this.selectedFile);
+    }
+  }
+
+  onSave() {
+    if (this.selectedFile) {
+      console.log('File to upload:', this.selectedFile);
+    }
+  }
 }

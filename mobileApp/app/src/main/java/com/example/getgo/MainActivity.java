64a970121177cfba0c6f7074b30d77c5892b31af
Meris <bilalovic.meris4444@gmail.com>
2025-12-23 02:34:15 +0100

    Add bottom navbar

--- mobileApp/app/src/main/java/com/example/getgo/MainActivity.java
+++ mobileApp/app/src/main/java/com/example/getgo/MainActivity.java
@@ -8,6 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.getgo.fragments.DriverHomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
+
+
 import com.example.getgo.fragments.RideHistoryFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,15 +22,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragmentContainer, new RideHistoryFragment())
                 .commit();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            if (item.getItemId() == R.id.item_1) {
+                selectedFragment = new DriverHomeFragment();
+            } else if (item.getItemId() == R.id.item_2) {
+                selectedFragment = new RideHistoryFragment();
+            } else if (item.getItemId() == R.id.item_3) {
+                selectedFragment = new DriverHomeFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainer, selectedFragment)
+                        .commit();
+            }
+
+            return true;
+        });
 
     }
 }

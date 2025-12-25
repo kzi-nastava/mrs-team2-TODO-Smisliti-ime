d2b46848bdf1a2a8bcd7baccc45d42bfcb2bda2f
Meris <bilalovic.meris4444@gmail.com>
2025-12-25 14:56:17 +0100

    Initial Spring Boot project structure

---
+++ backend/GetGo-BE/src/main/java/rs/getgo/backend/GetGoBeApplication.java
@@ -0,0 +1,13 @@
+package rs.getgo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetGoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetGoBeApplication.class, args);
+    }
+
+}

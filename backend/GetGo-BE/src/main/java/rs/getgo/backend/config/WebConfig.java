6db5faf7fcf1116a5b0750ee05c23c3f2e315908
Meris <bilalovic.meris4444@gmail.com>
2026-01-10 01:51:49 +0100

    Some map BE improvements

--- backend/GetGo-BE/src/main/java/rs/getgo/backend/config/WebConfig.java
+++ backend/GetGo-BE/src/main/java/rs/getgo/backend/config/WebConfig.java
@@ -1,4 +1,19 @@
 package rs.getgo.backend.config;
 
-public class WebConfig {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("http://localhost:*") // any port
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }

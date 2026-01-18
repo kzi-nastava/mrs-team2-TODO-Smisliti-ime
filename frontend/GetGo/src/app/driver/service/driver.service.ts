65bfa0ee998947205ca8e01d7272841ebf04b0ce
AnastazijaPetrov <anastazija.petrov004@gmail.com>
2026-01-18 16:09:59 +0100

    Connect driver profile to backend (#76)

--- frontend/GetGo/src/app/driver/service/driver.service.ts
+++ frontend/GetGo/src/app/driver/service/driver.service.ts
@@ -20,6 +20,46 @@ export interface SetPasswordResponseDTO {
   message: string;
 }
 
+export interface GetDriverDTO {
+  id: number;
+  email: string;
+  name: string;
+  surname: string;
+  phone: string;
+  address: string;
+  profilePictureUrl: string;
+  vehicleModel: string;
+  vehicleType: string;
+  vehicleLicensePlate: string;
+  vehicleSeats: number;
+  vehicleHasBabySeats: boolean;
+  vehicleAllowsPets: boolean;
+  recentHoursWorked: number;
+}
+
+export interface UpdateDriverPersonalDTO {
+  name: string;
+  surname: string;
+  phone: string;
+  address: string;
+}
+
+export interface UpdateDriverVehicleDTO {
+  vehicleModel: string;
+  vehicleType: string;
+  vehicleLicensePlate: string;
+  vehicleSeats: number;
+  vehicleHasBabySeats: boolean;
+  vehicleAllowsPets: boolean;
+}
+
+export interface CreatedDriverChangeRequestDTO {
+  requestId: number;
+  driverId: number;
+  status: string;
+  message: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -35,4 +75,23 @@ export class DriverService {
   setDriverPassword(data: SetPasswordDTO): Observable<SetPasswordResponseDTO> {
     return this.http.post<SetPasswordResponseDTO>(`${this.apiUrl}/activate`, data);
   }
+
+  getProfile(): Observable<GetDriverDTO> {
+      return this.http.get<GetDriverDTO>(`${this.apiUrl}/profile`);
+  }
+
+  requestPersonalInfoChange(data: UpdateDriverPersonalDTO): Observable<CreatedDriverChangeRequestDTO> {
+    return this.http.post<CreatedDriverChangeRequestDTO>(`${this.apiUrl}/profile/change-requests/personal`, data);
+  }
+
+  requestVehicleInfoChange(data: UpdateDriverVehicleDTO): Observable<CreatedDriverChangeRequestDTO> {
+    return this.http.post<CreatedDriverChangeRequestDTO>(`${this.apiUrl}/profile/change-requests/vehicle`, data);
+  }
+
+  requestProfilePictureChange(file: File): Observable<CreatedDriverChangeRequestDTO> {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<CreatedDriverChangeRequestDTO>(`${this.apiUrl}/profile/change-requests/picture`, formData);
+  }
+
 }

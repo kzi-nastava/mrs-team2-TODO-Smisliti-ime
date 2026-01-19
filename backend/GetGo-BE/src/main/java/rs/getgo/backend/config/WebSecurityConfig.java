7289549190810a051afff25a95d1d919bd51807c
Meris <bilalovic.meris4444@gmail.com>
2026-01-19 01:42:44 +0100

    BE-Finish implementation of ride tracking info box

--- backend/GetGo-BE/src/main/java/rs/getgo/backend/config/WebSecurityConfig.java
+++ backend/GetGo-BE/src/main/java/rs/getgo/backend/config/WebSecurityConfig.java
@@ -91,6 +91,7 @@ public class WebSecurityConfig {
                     .requestMatchers(new AntPathRequestMatcher("/uploads/*")).permitAll()
                     .requestMatchers(new AntPathRequestMatcher("/api/ratings/**")).permitAll() // Added for test
                     .requestMatchers(new AntPathRequestMatcher("/api/drivers/**")).permitAll() // Added for test
+                    .requestMatchers(new AntPathRequestMatcher("/api/rides/**")).permitAll() // Added for test
                     .requestMatchers("/error").permitAll()
                     .anyRequest().authenticated();
         });

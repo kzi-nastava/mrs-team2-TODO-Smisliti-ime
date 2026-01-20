1d153a287319d713d495389beb398decba04b0e8
Meris <bilalovic.meris4444@gmail.com>
2026-01-20 05:48:03 +0100

    Implement loading cars from database

--- mobileApp/app/src/main/java/com/example/getgo/fragments/GuestHomeFragment.java
+++ mobileApp/app/src/main/java/com/example/getgo/fragments/GuestHomeFragment.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

6743585630d0e6ccafc17ad65626d2d473ad84d3
AnastazijaPetrov <anastazija.petrov004@gmail.com>
2026-01-23 00:41:16 +0100

    Add web sockets (#102)

---
+++ backend/GetGo-BE/src/main/java/rs/getgo/backend/controllers/WebSocketController.java
@@ -0,0 +1,46 @@
+package rs.getgo.backend.controllers;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import rs.getgo.backend.dtos.driver.GetDriverLocationDTO;
+import rs.getgo.backend.dtos.ride.GetDriverActiveRideDTO;
+
+@Controller
+public class WebSocketController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public WebSocketController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    /**
+     * Notify specific driver about ride assignment
+     */
+    public void notifyDriverRideAssigned(Long driverId, GetDriverActiveRideDTO rideDTO) {
+        messagingTemplate.convertAndSend(
+                "/socket-publisher/driver/" + driverId + "/ride-assigned",
+                rideDTO
+        );
+    }
+
+    /**
+     * Broadcast driver location update
+     */
+    public void broadcastDriverLocation(Long driverId, GetDriverLocationDTO locationDTO) {
+        messagingTemplate.convertAndSend(
+                "/socket-publisher/driver/" + driverId + "/location",
+                locationDTO
+        );
+    }
+
+    /**
+     * Broadcast driver location to ride (for passengers tracking)
+     */
+    public void broadcastDriverLocationToRide(Long rideId, GetDriverLocationDTO locationDTO) {
+        messagingTemplate.convertAndSend(
+                "/socket-publisher/ride/" + rideId + "/driver-location",
+                locationDTO
+        );
+    }
+}

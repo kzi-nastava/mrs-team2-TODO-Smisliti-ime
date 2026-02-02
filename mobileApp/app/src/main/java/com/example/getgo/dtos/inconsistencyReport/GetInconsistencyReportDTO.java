7987ed2e417986eac08f8010eb3f610e20ffd271
Meris <bilalovic.meris4444@gmail.com>
2026-02-02 05:01:12 +0100

    Implement showing inconsistency reports in RideDetailFragment

--- mobileApp/app/src/main/java/com/example/getgo/dtos/inconsistencyReport/GetInconsistencyReportDTO.java
+++ mobileApp/app/src/main/java/com/example/getgo/dtos/inconsistencyReport/GetInconsistencyReportDTO.java
@@ -1,4 +1,28 @@
 package com.example.getgo.dtos.inconsistencyReport;
 
 public class GetInconsistencyReportDTO {
+
+    private Long id;
+    private String createdAt;
+    private String passengerEmail;
+    private String text;
+
+    public GetInconsistencyReportDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getPassengerEmail() {
+        return passengerEmail;
+    }
+
+    public String getText() {
+        return text;
+    }
 }

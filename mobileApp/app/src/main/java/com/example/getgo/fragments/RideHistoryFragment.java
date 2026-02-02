a8d9e7d7a5d4a3ad4130ef54ce68bdc5caf5ad00
Meris <bilalovic.meris4444@gmail.com>
2026-02-02 02:54:43 +0100

    Implement ride history pagination

--- mobileApp/app/src/main/java/com/example/getgo/fragments/RideHistoryFragment.java
+++ mobileApp/app/src/main/java/com/example/getgo/fragments/RideHistoryFragment.java
@@ -8,7 +8,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.getgo.R;
@@ -19,6 +22,7 @@ import java.util.List;
 import com.example.getgo.adapters.RideHistoryAdapter;
 import com.example.getgo.api.ApiClient;
 import com.example.getgo.dtos.ride.GetRideDTO;
+import com.example.getgo.dtos.ride.PageResponse;
 import com.example.getgo.interfaces.DriverApi;
 import com.example.getgo.model.Ride;
 
@@ -29,6 +33,9 @@ import java.util.Locale;
 import java.util.Date;
 import java.time.format.DateTimeFormatter;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class RideHistoryFragment extends Fragment {
@@ -38,6 +45,11 @@ public class RideHistoryFragment extends Fragment {
     private TextView tvFilterDate;
     private ArrayList<GetRideDTO> fullHistoryList;
     private MaterialButton btnReset, btnApply;
+    private MaterialButton btnNext, btnPrev;
+
+    private int currentPage = 0;
+    private int pageSize = 5;
+    private int totalElements = 0;
 
 
     public RideHistoryFragment() {
@@ -73,10 +85,35 @@ public class RideHistoryFragment extends Fragment {
 
         btnReset = view.findViewById(R.id.btnReset);
         btnApply = view.findViewById(R.id.btnApply);
+        btnNext = view.findViewById(R.id.btnNextPage);
+        btnPrev = view.findViewById(R.id.btnPrevPage);
 
         tvFilterDate = view.findViewById(R.id.tvFilterDate);
 
         rideHistoryLV = view.findViewById(R.id.rideHistoryListView);
+
+        Spinner spinnerPageSize = view.findViewById(R.id.spinnerPageSize);
+        Integer[] pageSizes = {5, 10, 20};
+        ArrayAdapter<Integer> adapterSpinner = new ArrayAdapter<>(requireContext(),
+                android.R.layout.simple_spinner_item, pageSizes);
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPageSize.setAdapter(adapterSpinner);
+
+        spinnerPageSize.setSelection(0);
+
+        // when passenger change number of cards per page
+        spinnerPageSize.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                pageSize = pageSizes[position];
+                currentPage = 0; // reset to first page
+                loadRideHistoryFromServer();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
 
         fullHistoryList = new ArrayList<>();
 
@@ -87,18 +124,30 @@ public class RideHistoryFragment extends Fragment {
 
         btnReset.setOnClickListener(v -> {
             tvFilterDate.setText("Filter by date");
-            adapter.setRides(new ArrayList<>(fullHistoryList));
+            currentPage = 0;
+            loadRideHistoryFromServer();
         });
 
         btnApply.setOnClickListener(v -> {
-            String selectedDate = tvFilterDate.getText().toString();
-            if (!selectedDate.equals("Filter by date")) {
-                filterRidesByDate(selectedDate);
+            currentPage = 0;
+            loadRideHistoryFromServer();
+        });
+
+        btnNext.setOnClickListener(v -> {
+            if ((currentPage + 1) * pageSize < totalElements) {
+                currentPage++;
+                loadRideHistoryFromServer();
             }
         });
 
-        Long driverId = 11L;
-        loadRideHistoryFromServer(driverId);
+        btnPrev.setOnClickListener(v -> {
+            if (currentPage > 0) {
+                currentPage--;
+                loadRideHistoryFromServer();
+            }
+        });
+
+        loadRideHistoryFromServer();
 
         adapter.setOnRideClickListener(ride -> {
             RideDetailFragment fragment =
@@ -149,32 +198,45 @@ public class RideHistoryFragment extends Fragment {
         adapter.setRides(filteredList);
     }
 
-    private void loadRideHistoryFromServer(Long driverId) {
+    private void loadRideHistoryFromServer() {
         DriverApi driverApi = ApiClient.getClient().create(DriverApi.class);
+        String startDate = "";
+//        String startDate = "01-02-2026";
 
-        driverApi.getDriverRides(driverId).enqueue(new retrofit2.Callback<List<GetRideDTO>>() {
+        if (!tvFilterDate.getText().toString().equals("Filter by date")) {
+            try {
+                SimpleDateFormat inputFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+                SimpleDateFormat outputFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                Date date = inputFormat.parse(tvFilterDate.getText().toString());
+                startDate = outputFormat.format(date);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        driverApi.getDriverRides(currentPage, pageSize, startDate).enqueue(new Callback<PageResponse<GetRideDTO>>() {
             @Override
-            public void onResponse(retrofit2.Call<List<GetRideDTO>> call, retrofit2.Response<List<GetRideDTO>> response) {
+            public void onResponse(Call<PageResponse<GetRideDTO>> call, Response<PageResponse<GetRideDTO>> response) {
                 if (response.isSuccessful() && response.body() != null) {
-                    List<GetRideDTO> ridesFromServer = response.body();
+                    PageResponse<GetRideDTO> pageResponse = response.body();
+
+                    List<GetRideDTO> ridesFromServer = pageResponse.getContent();
+                    totalElements = pageResponse.getTotalElements();
 
                     Log.d("RideHistory", "Broj vožnji preuzetih sa servera: " + ridesFromServer.size());
 
                     for (GetRideDTO ride : ridesFromServer) {
                         Log.d("RideHistory", "Ride: " + ride.getStartPoint() + " -> " + ride.getEndPoint());
                     }
-
-                    adapter.setRides(ridesFromServer);
-
-
                     fullHistoryList = new ArrayList<>(ridesFromServer);
+                    adapter.setRides(ridesFromServer);
                 } else {
                     Log.d("RideHistory", "Response nije uspešan: " + response.code());
                 }
             }
 
             @Override
-            public void onFailure(retrofit2.Call<List<GetRideDTO>> call, Throwable t) {
+            public void onFailure(Call<PageResponse<GetRideDTO>> call, Throwable t) {
                 t.printStackTrace();
                 Log.d("RideHistory", "Greška pri preuzimanju vožnji: " + t.getMessage());
 

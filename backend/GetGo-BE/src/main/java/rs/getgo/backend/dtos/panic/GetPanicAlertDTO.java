9b67c025c330351a04e38b71b44ef34f0879c5e7
AnastazijaPetrov <anastazija.petrov004@gmail.com>
2026-02-05 01:59:53 +0100

    Update web sockets to use DTOs

---
+++ backend/GetGo-BE/src/main/java/rs/getgo/backend/dtos/panic/GetPanicAlertDTO.java
@@ -0,0 +1,20 @@
+package rs.getgo.backend.dtos.panic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class GetPanicAlertDTO {
+    private Long rideId;
+    private Long userId;
+    private String userEmail;
+    private LocalDateTime triggeredAt;
+    private String message;
+}
